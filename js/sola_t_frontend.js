81e35ed8b45e8dc0774de8f08948776ece7b4ddc
SolaPlugins <SolaPlugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-24 06:05:45 +0000

    * 1.2 2014-11-24  * Fixed bug that caused a Fatal error on the testimonials page
    git-svn-id: https://plugins.svn.wordpress.org/sola-testimonials/tags/1.1@1031528 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/sola_t_frontend.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function() {
+
+    jQuery.validate({
+        form: "#sola_t_submit_testimonial"}
+    );
+
+    jQuery(".sola_t_image").imgLiquid({
+        fill: true,
+        horizontalAlign: "center",
+        verticalAlign: "top"
+    });
+
+
+});
